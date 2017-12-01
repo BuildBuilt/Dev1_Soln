727452840744039ec9e7bfbfa29b9b957f64d659
BuildBuilt <buildbuilt.ca@gmail.com>
2017-12-01 14:28:06 +0700

    Add project files.

---
+++ Dev1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dev 1: version 1");
+            Console.WriteLine("Dev 1: version 1 - change 1");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
